efa720c10429b155ac04dd443ce14aaead68a403
Levko Nikitin <53998902+BoringLily@users.noreply.github.com>
2023-02-21 18:39:09 -0500

    Completed Step 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import Calendar from './components/Calendar'
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,7 +12,7 @@ function App() {
     <div className="App">
     <h1> 7 days to startup</h1>
     <h2> Here is a week plan for how to build your dream SaaS startup.</h2>
-
+    <Calendar />
     </div>
   )
 }
