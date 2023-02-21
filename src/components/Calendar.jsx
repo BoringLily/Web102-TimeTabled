639378810ff0cafc361515c4adf00bc45fbaf4ea
Levko Nikitin <53998902+BoringLily@users.noreply.github.com>
2023-02-21 18:48:05 -0500

    Completed Step 2

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,9 +2,127 @@ import React from "react";
 
 
 const Calendar = () => {
-    
+
     return (
-        <div className="Calendar"> "Testing the calendar component" </div>
+        <div className="Calendar">
+<table>
+  <thead>
+       <tr>
+          <th></th>
+          <th>Monday</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+          <th>Sunday</th>
+      </tr>         
+  </thead>
+  <tbody>
+  <tr>
+          <td className="time"> 08:00 am</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 09:00 am</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 10:00 am</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 11:00 am</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 12:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 01:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 02:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 03:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 04:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+      <tr>
+          <td className="time"> 05:00 pm</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+      </tr>
+
+  </tbody>    
+</table>
+        </div>
 
     )
 }
